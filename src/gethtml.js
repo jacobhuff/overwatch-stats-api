a882301db93fc27bfa212cf0fe7db3e25692a90a
Jacob Huff <jacobhuff1313@gmail.com>
2019-11-19 13:24:20 -0500

    Switched to Axios

--- src/gethtml.js
+++ src/gethtml.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const fetch = require('node-fetch')
+const axios = require('axios')
 	, cheerio = require('cheerio');
 
 module.exports = async (battletag, platform) => {
 	
 	const url = encodeURI(`/api?url=https://playoverwatch.com/en-us/career/${platform}/${battletag}`);
 	
-	const html = await fetch(url);
-	
-	return html;
+	const res = await axios.get(url);
+
+	return res.data;
 }
